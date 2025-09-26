f37750b7b90a05273c4ec9264d439fb7fe6a446d
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-09-26 08:17:54 -0500

    added wc.config.js

--- wc.config.js
+++ wc.config.js
@@ -2,12 +2,12 @@ export default {
   libraries: {
     "@awesome.me/webawesome": {
       /**
-       * Fetch manifest from a URL
-       * This isn't needed if you have the NPM package installed
+       * Fetch the Custom Elements Manifest file for Web Awesome from a URL.
+       * This isn't needed if the @awesome.me/webawesome package is installed.
        * See https://github.com/wc-toolkit/wc-language-server/issues/27.
        */
       manifestSrc:
-        "[./build/custom-elements.json](https://cdn.jsdelivr.net/npm/@awesome.me/webawesome@3.0.0-beta.6/dist/custom-elements.json)",
+        "https://cdn.jsdelivr.net/npm/@awesome.me/webawesome@3.0.0-beta.4/dist/custom-elements.json",
     },
   },
 };
